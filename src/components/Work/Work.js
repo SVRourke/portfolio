f678956568ae71d7c4e1fd6224dc9b7aa85a2c9a
SVRourke <samuelvanrourke@gmail.com>
2021-05-10 08:37:22 -0400

    began work component

---
+++ src/components/Work/Work.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Work.scss'
+
+const Work = () => {
+  return (
+    <section className={"main-container column about"}>
+      <div className={"about-row row"}>
+        <div className={"about-left column"}>
+          <h2>My Work:</h2>
+        </div>
+
+        <div className={"about-right column"}>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Work;
