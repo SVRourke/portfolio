991b374398f21f5d9584f26d64e130615657891a
SVRourke <samuelvanrourke@gmail.com>
2021-05-10 07:19:43 -0400

    stubbed out header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <div className={"main-container"}>
+            <h1>Hi, I'm Sam</h1>
+            <h2>
+              I'm passionate about creating tools
+              that make people's lives easier
+            </h2>
+        </div>
+    );
+}
+
+export default Header;
+
