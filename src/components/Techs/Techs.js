47117f65942b790885405268841219f82f88fdfb
SVRourke <samuelvanrourke@gmail.com>
2021-05-10 13:54:56 -0400

    created techs component

---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Techs = () => {
+  return (
+    <section className={"main-container column techs"}>
+      <h2>what i use:</h2>
+      <div className={"techs-row row"}>
+      </div>
+    </section>
+  );
+}
+
+export default Techs;
