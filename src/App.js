ed436a7e38315544c0beb41a085f486bdc3c770a
SVRourke <samuelvanrourke@gmail.com>
2021-05-10 08:34:08 -0400

    added About and Work components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import Header from './components/Header/Header'
 import NavBar from './components/Navbar'
+import About from './components/About/About'
+import Work from './components/Work/Work'
 
 function App() {
   return (
     <>
       <NavBar />
       <Header />
+      <About />
+      <Work />
     </>
   );
 }
