d126634b804d47f11f11c30935b0fcd01decd48e
SVRourke <samuelvanrourke@gmail.com>
2021-05-10 08:36:25 -0400

    reworked header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import styled from 'styled-components';
+import './header.scss'
 
 const Header = () => {
-    return (
-        <div className={"main-container"}>
-            <h1>Hi, I'm Sam</h1>
-            <h2>
-              I'm passionate about creating tools
-              that make people's lives easier
-            </h2>
+  return (
+    <header className={"main-container column header"}>
+      <div className={'header-row row'}>
+
+        <div className={'column header-left'}>
+            <div className={'pic'} />
         </div>
-    );
+
+        <div className={'column header-right'}>
+          <h1>Hi, I'm Sam</h1>
+          <h2>
+            I'm passionate about creating tools
+            that make managing your life easier.
+          </h2>
+        </div>
+
+      </div>
+    </header>
+  );
 }
 
 export default Header;
-
