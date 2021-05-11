7aa8a38ed010b886d85b478ee4d987dd44109e30
SVRourke <samuelvanrourke@gmail.com>
2021-05-11 14:33:07 -0400

    imported new components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import NavBar from './components/Navbar'
 import About from './components/About/About'
 import Work from './components/Work/Work'
 import Techs from './components/Techs/Techs'
+import BlogSample from './components/BlogSample/BlogSample'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <About />
       <Work />
       <Techs />
+      <BlogSample />
+      <Contact />
     </>
   );
 }
