74bf27ff2a1e9703b78e5690b4567ea8cebbbbac
SVRourke <samuelvanrourke@gmail.com>
2021-05-12 09:59:43 -0400

    created index to simplify imports

---
+++ src/blocks/index.js
@@ -0,0 +1,19 @@
+import Header from './Header/Header'
+import NavBar from './Navbar'
+import About from './About/About'
+import Work from './Work/Work'
+import Techs from './Techs/Techs'
+import BlogSample from './BlogSample/BlogSample'
+import Contact from './Contact/Contact'
+import Footer from './Footer/Footer'
+
+export {
+  Header,
+  NavBar,
+  About,
+  Work,
+  Techs,
+  BlogSample,
+  Contact,
+  Footer
+}
