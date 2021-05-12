fe44fe3de3259d969dfa8b01adb252e4d003e2fe
SVRourke <samuelvanrourke@gmail.com>
2021-05-12 09:59:26 -0400

    simplified imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
-import Header from './components/Header/Header'
-import NavBar from './components/Navbar'
-import About from './components/About/About'
-import Work from './components/Work/Work'
-import Techs from './components/Techs/Techs'
-import BlogSample from './components/BlogSample/BlogSample'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
+
+import {
+  Header,
+  NavBar,
+  About,
+  Work,
+  Techs,
+  BlogSample,
+  Contact,
+  Footer
+} from 'blocks'
+
 function App() {
   return (
     <>
