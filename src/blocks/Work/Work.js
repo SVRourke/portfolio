90f269d94b58c94bced2580ded1baf0764334c76
SVRourke <samuelvanrourke@gmail.com>
2021-05-19 08:29:13 -0400

    implemented slide logic (needs animations)

--- src/blocks/Work/Work.js
+++ src/blocks/Work/Work.js
@@ -1,42 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Work.scss'
+import Slide from './Slide'
+
+import {projects} from './testData'
 
 const Work = () => {
+  const [idx, setIdx] = useState(0) 
+  const sorted = projects.sort((a, b) => b.date - a.date)
+
+  const nextSlide = () => {
+    idx !== sorted.length - 1
+      ? setIdx(idx + 1)
+      : setIdx(0)
+  }
+
+  const prevSlide = () => {
+    idx !== 0
+      ? setIdx(idx - 1)
+      : setIdx(sorted.length - 1)
+  }
   return (
     <section className={"main-container column work"}>
-      <h2>my work:</h2>
-      <div className={"work-row row"}>
-        <div className={"work-card"} >
-          <h1>Forms</h1>
-        </div>
+      <h2 id='project-title'>my work:</h2>
+      <Slide project={projects[idx]} idx={idx + 1} total={sorted.length} />
 
-        <div className={"work-card"} >
-          <h1>Pantry</h1>
-        </div>
-
-        <div className={"work-card"} >
-          <h1>Kanban</h1>
-        </div>
-
-        <div className={"work-card"} >
-          <h1>Quizzlet</h1>
-        </div>
-
-        <div className={"work-card"} >
-          <h1>Dice</h1>
-        </div>
-
-        <div className={"work-card"} >
-          <h1>Ultima Tec</h1>
-        </div>
-
-        <div className={"work-card"} >
-          <h1>Trading Unlimited</h1>
-        </div>
-
-        <div className={"work-card"} >
-          <h1>Portfolio</h1>
-        </div>
+      <div className={'work-controls row'} >
+        <button onClick={prevSlide} >prev</button>
+        <p>{idx + 1} of {sorted.length}</p>
+        <button onClick={nextSlide} >next</button>
       </div>
     </section>
   );
