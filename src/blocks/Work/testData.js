72f79da5ef223d544d07809e9991a1f0b4759326
SVRourke <samuelvanrourke@gmail.com>
2021-05-19 08:28:54 -0400

    created testData to simulate info from cms

---
+++ src/blocks/Work/testData.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Pantry",
+    date: "2021-05-20",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet. Sit amet mattis vulputate enim nulla aliquet porttitor. Consectetur a erat nam at lectus urna duis convallis. Magnis dis parturient montes nascetur ridiculus",
+    postUrl: "#",
+    githubUrl: "#"
+  },
+  {
+    title: "TradingUnlimited",
+    date: "2021-05-19",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet. Sit amet mattis vulputate enim nulla aliquet porttitor. Consectetur a erat nam at lectus urna duis convallis. Magnis dis parturient montes nascetur ridiculus",
+    postUrl: "#",
+    githubUrl: "#"
+  },
+  {
+    title: "UltimaTec",
+    date: "2021-04-30",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet. Sit amet mattis vulputate enim nulla aliquet porttitor. Consectetur a erat nam at lectus urna duis convallis. Magnis dis parturient montes nascetur ridiculus",
+    postUrl: "#",
+    githubUrl: "#"
+  },
+  {
+    title: "Quizzical",
+    date: "2021-04-20",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet. Sit amet mattis vulputate enim nulla aliquet porttitor. Consectetur a erat nam at lectus urna duis convallis. Magnis dis parturient montes nascetur ridiculus",
+    postUrl: "#",
+    githubUrl: "#"
+  },
+  {
+    title: "Kanban Board",
+    date: "2021-02-13",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet. Sit amet mattis vulputate enim nulla aliquet porttitor. Consectetur a erat nam at lectus urna duis convallis. Magnis dis parturient montes nascetur ridiculus",
+    postUrl: "#",
+    githubUrl: "#"
+  },
+  {
+    title: "fetch-contribution-calendar",
+    date: "2021-01-25",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet. Sit amet mattis vulputate enim nulla aliquet porttitor. Consectetur a erat nam at lectus urna duis convallis. Magnis dis parturient montes nascetur ridiculus",
+    postUrl: "#",
+    githubUrl: "#"
+  },
+]
