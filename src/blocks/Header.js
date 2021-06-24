f9c3f4a9afcfe73ccc9bc9e6396f3adbfa57aed0
SVRourke <samuelvanrourke@gmail.com>
2021-06-24 09:09:28 -0400

    format, remove class from header convert to grid layout

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import 'styles/Header.scss'
-import { Fade } from 'react-awesome-reveal'
+import React from "react";
+import "styles/Header.scss";
+import { Fade } from "react-awesome-reveal";
 
 export default function Header() {
-    return (
-        <header className={'block col'}>
-            <Fade className='hero-word hi' direction={'down'} >
-                <span id='hi'>Hi,</span>
-            </Fade>
+  return (
+    // <header className={}>
+    <header>
+      <Fade className="hi" direction={"down"}>
+        <span id="hi">Hi,</span>
+      </Fade>
 
-            <Fade className='hero-word sam' direction={'right'} duration={1000} delay={600}>
-                <span id='sam'>Sam.</span>
-            </Fade>
-            <Fade className='hero-word im' direction={'left'} delay={200} >
-                <span className='outline-title'>I'm</span>
-            </Fade>
-        </header>
-    )
+      <Fade className="sam" direction={"up"} delay={1000}>
+        <span id="sam">Sam.</span>
+      </Fade>
+      <Fade className="hero-word im" direction={"left"} delay={200}>
+        <span className="outline-title">I'm</span>
+      </Fade>
+    </header>
+  );
 }
