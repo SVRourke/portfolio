ab6fd9a8a053745ee9d4917c5c981369ac0f7425
SVRourke <samuelvanrourke@gmail.com>
2021-06-26 07:47:32 -0400

    import stylesheet, and fade

--- src/blocks/Projects.js
+++ src/blocks/Projects.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+import "styles/Projects.scss";
+import { Fade } from "react-awesome-reveal";
+
 export default function Projects() {
-  return <section></section>;
+  return (
+      <section className='projects'>
+        
+
+      </section>;
+  ) 
 }
