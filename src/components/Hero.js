2db29d7187b75fd2d7e9aeec627fddfdc2066215
Samuel Rourke <samuelvanrourke@gmail.com>
2021-07-11 22:15:14 -0400

    stub navbar, text content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "styles/components/hero.scss";
 
 export default function Hero() {
   return (
     <div id="hero">
-      <h1>Hi I'm Sam</h1>
+      <div className="hero-navBar">
+        <a>svrourke</a>
+        <a>about</a>
+        <a>work</a>
+        <a>blog</a>
+        <a>contact</a>
+      </div>
+      <div className="hero-textbox">
+        <span>✌</span>
+        <h1>Hi I'm Sam</h1>
+        <p>
+          I'm passionate about creating tools that provide value and make life
+          easier for people.
+        </p>
+      </div>
+      <div className="hero-imagePlaceholder"></div>
     </div>
   );
 }
