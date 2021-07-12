6ab567ec3d75d12ec0376c13e90f6f818dcee3f9
Samuel Rourke <samuelvanrourke@gmail.com>
2021-07-11 21:00:10 -0400

    cleared

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "styles/App.scss";
 
-import Header from "blocks/Header";
-import About from "blocks/About";
-import Projects from "blocks/Projects";
 function App() {
-  return (
-    <>
-      <Header />
-      <About />
-      <Projects />
-    </>
-  );
+  return <p>Hello</p>;
 }
 
 export default App;
