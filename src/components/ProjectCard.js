d18b1eb1b1a854b788b011acfc4c3bdb0d159f81
SVRourke <samuelvanrourke@gmail.com>
2021-07-14 08:37:28 -0400

    create ProjectCard component and stylesheet

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "styles/components/ProjectCard.scss";
+
+export default function ProjectCard() {
+  return (
+    <div class="project-card">
+      <p>Project Card</p>
+    </div>
+  );
+}
