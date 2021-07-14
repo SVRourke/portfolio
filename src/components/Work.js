d8a090e7340327a8391c2471ef9980be3a71599f
SVRourke <samuelvanrourke@gmail.com>
2021-07-14 08:38:24 -0400

    add github link, add projectCard stubs for styling

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,29 @@
 import React from "react";
+
+import ProjectCard from "./ProjectCard";
+
 import "styles/components/Work.scss";
 
 export default function Work() {
   return (
     <section id="work">
       <h2>i like to make stuff 📐</h2>
+
+      <div class="work-grid">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+
+      <a class="github-link" href="https://www.github.com/SVRourke">
+        my github >
+      </a>
     </section>
   );
 }
