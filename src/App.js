8b6d567dcfc8b0abf84444a8fb871107c1665095
SVRourke <samuelvanrourke@gmail.com>
2021-07-14 07:54:37 -0400

    create work component, stylesheet, and add to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "styles/App.scss";
 import Hero from "components/Hero";
 import About from "components/About";
+import Work from "components/Work";
 function App() {
   return (
     <>
       <Hero />
       <About />
+      <Work />
     </>
   );
 }
---
+++ src/components/Work.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "styles/components/Work.scss";
+
+export default function Work() {
+  return (
+    <section id="work">
+      <h2>i like to make stuff 📐</h2>
+    </section>
+  );
+}
