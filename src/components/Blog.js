619a335f293a412fa860c2610975342adbe5843d
SVRourke <samuelvanrourke@gmail.com>
2021-07-15 15:00:36 -0400

    change root blog element to section, update block heading text

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,8 +3,8 @@ import "styles/components/Blog.scss";
 
 export default function Blog() {
   return (
-    <div>
-      <h2>Blog</h2>
-    </div>
+    <section id="blog">
+      <h2>i like to make stuff</h2>
+    </section>
   );
 }
