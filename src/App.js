ca762e36ade9f3a33450d2f9c3bc009d1092c895
SVRourke <samuelvanrourke@gmail.com>
2021-07-15 14:56:53 -0400

    stub out blog component and stylesheet, add to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "styles/App.scss";
 import Hero from "components/Hero";
 import About from "components/About";
 import Work from "components/Work";
+import Blog from "components/Blog";
+
 function App() {
   return (
     <>
       <Hero />
       <About />
       <Work />
+      <Blog />
     </>
   );
 }
---
+++ src/components/Blog.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "styles/components/Blog.scss";
+
+export default function Blog() {
+  return (
+    <div>
+      <h2>Blog</h2>
+    </div>
+  );
+}
