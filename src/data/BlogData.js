439e6b1ffe469ca025745719ba7f59ab4eb6e5c5
SVRourke <samuelvanrourke@gmail.com>
2021-07-16 08:36:33 -0400

    created a file for storing temp data in json format (later to be replaced by a call to graphCMS

---
+++ src/data/BlogData.js
@@ -0,0 +1,53 @@
+const posts = [
+  {
+    title: "My top 10 must-download vscode extensions",
+    description:
+      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
+    date: "09-07-21",
+    url: "#hero",
+    img: "",
+  },
+  {
+    title: "Building the first version of Pantry",
+    description:
+      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
+    date: "04-07-21",
+    url: "#hero",
+    img: "",
+  },
+
+  {
+    title: "This, hoisting and JavaScript execution contexts",
+    description:
+      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
+    date: "20-06-21",
+    url: "#hero",
+    img: "",
+  },
+  {
+    title: "Learning JavaScript by copying tools you use",
+    description:
+      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
+    date: "10-06-21",
+    url: "#hero",
+    img: "",
+  },
+  {
+    title: "Building a quiz app with ruby on rails",
+    description:
+      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
+    date: "04-05-21",
+    url: "#hero",
+    img: "",
+  },
+  {
+    title: "Building a Jira clone with sinatra",
+    description:
+      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
+    date: "26-04-21",
+    url: "#hero",
+    img: "",
+  },
+];
+
+export default posts;
