bb46dd7308415caf22b0aec64b20e70cf21ef9ff
SVRourke <samuelvanrourke@gmail.com>
2021-07-16 12:36:06 -0400

    add real data

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,52 +1,51 @@
 const posts = [
   {
-    title: "My top 10 must-download vscode extensions",
+    title: "How to install Notion on Ubuntu",
     description:
-      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
+      "In recent months I’ve fallen in love with Notion. For the past few years I’ve used windows purely for gaming, I’m just more comfortable working with Linux. So when I bought a shiny new ThinkPad and immediately installed Ubuntu It finally felt right, and everything fell perfectly into place, that is until I decided to install notion.",
     date: "09-07-21",
-    url: "#hero",
-    img: "",
+    url: "https://svrourke.medium.com/how-to-install-notion-on-ubuntu-bb4597dd7175",
+    img: "https://miro.medium.com/max/700/1*n9ew3qpaVkhxCYQpbrN4cQ.png",
   },
   {
-    title: "Building the first version of Pantry",
-    description:
-      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
-    date: "04-07-21",
-    url: "#hero",
-    img: "",
+    title: "My top 10 must-have vscode extensions",
+    description: "Mostly just pretty stuff",
+    date: "09-07-21",
+    url: "https://svrourke.medium.com/building-the-first-version-of-pantry-cf110f5a592b",
+    img: "https://svrourke.medium.com/my-5-must-have-vs-code-extensions-6c4d74474068",
   },
 
   {
-    title: "This, hoisting and JavaScript execution contexts",
+    title: "Building the first version of Pantry.",
     description:
-      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
-    date: "20-06-21",
-    url: "#hero",
-    img: "",
+      "There’s a problem I’ve faced which I’m sure is familiar to most people: you’re heading home from the grocery store",
+    date: "25-06-21",
+    url: "https://svrourke.medium.com/building-the-first-version-of-pantry-cf110f5a592b",
+    img: "https://miro.medium.com/max/5184/0*PzUY2w388JKnLW7i",
   },
   {
-    title: "Learning JavaScript by copying tools you use",
+    title: "This, Hoisting and Execution Context in JavaScript",
     description:
-      "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
-    date: "10-06-21",
-    url: "#hero",
-    img: "",
+      "If you’ve been learning JavaScript you’ve probably run into “this” a few times, usually with a minimal explanation followed by something along the lines “don’t worry about how this works it’ll make more sense later on”. Well now it’s later on, so what is this?",
+    date: "28-02-21",
+    url: "https://javascript.plainenglish.io/this-hoisting-and-execution-context-in-javascript-f508a261691f",
+    img: "https://miro.medium.com/max/700/0*svYUaST5g7amW7WR",
   },
   {
-    title: "Building a quiz app with ruby on rails",
+    title: "Learning JavaScript by roughly cloning Coolors",
     description:
       "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
-    date: "04-05-21",
-    url: "#hero",
-    img: "",
+    date: "09-02-21",
+    url: "https://medium.com/geekculture/learning-javascript-by-roughly-cloning-coolors-20115cab8a8e",
+    img: "https://miro.medium.com/max/700/1*hASzyHEXJURzxHa47tpTeg.png",
   },
   {
-    title: "Building a Jira clone with sinatra",
+    title: "Building a Quiz app with Ruby on Rails",
     description:
       "Ex officia Lorem enim irure. Ea ea in sit eu eu deserunt duis. Non dolor duis non dolor.",
-    date: "26-04-21",
-    url: "#hero",
-    img: "",
+    date: "24-12-20",
+    url: "https://svrourke.medium.com/building-a-quiz-app-with-ruby-on-rails-896a22615b3",
+    img: "https://miro.medium.com/max/700/1*xoVov9aVnlc_WDQILbIC_A.png",
   },
 ];
 
