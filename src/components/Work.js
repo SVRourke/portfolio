6d902adbc51aed4b34463862ec9e447ee8d6887c
SVRourke <samuelvanrourke@gmail.com>
2021-08-02 09:37:58 -0400

    use getProjects to replace hardcoded data

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,60 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import { getProjects } from "../cms/interface";
 import "styles/components/Work.scss";
 
-const projects = [
-  {
-    name: "Pantry",
-    blog: "medium.com",
-    repo: "github.com",
-    live: "svrourke.com",
-    image: "https://image.pngaaa.com/930/2507930-middle.png",
-  },
-  {
-    name: "Trading Unlimited",
-    blog: "medium.com",
-    repo: "github.com",
-    live: "svrourke.com",
-    image: "https://image.pngaaa.com/930/2507930-middle.png",
-  },
-  {
-    name: "fetch-contributions-calendar",
-    blog: "medium.com",
-    repo: "github.com",
-    live: "svrourke.com",
-    image: "https://image.pngaaa.com/930/2507930-middle.png",
-  },
-  {
-    name: "Quizzical",
-    blog: "medium.com",
-    repo: "github.com",
-    live: "svrourke.com",
-    image: "https://image.pngaaa.com/930/2507930-middle.png",
-  },
-  {
-    name: "Kanban",
-    blog: "medium.com",
-    repo: "github.com",
-    live: "svrourke.com",
-    image: "https://image.pngaaa.com/930/2507930-middle.png",
-  },
-  {
-    name: "Coolors Clone",
-    blog: "medium.com",
-    repo: "github.com",
-    live: "svrourke.com",
-    image: "https://image.pngaaa.com/930/2507930-middle.png",
-  },
-  {
-    name: "Todoist Clone",
-    blog: "medium.com",
-    repo: "github.com",
-    live: "svrourke.com",
-    image: "https://image.pngaaa.com/930/2507930-middle.png",
-  },
-];
-
 export default function Work() {
+  const [projects, setProjects] = useState([]);
+
+  getProjects().then((d) => {
+    setProjects(d["portfolioProjects"]);
+  });
+
   const cards = projects.map((project) => <ProjectCard data={project} />);
   return (
     <section id="work">
