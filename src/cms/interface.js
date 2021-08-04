49e01305add599bccf4a32aa660966d7c3ce2aa3
Samuel Rourke <samuelvanrourke@gmail.com>
2021-08-04 08:42:36 -0400

    update for new content schema

--- src/cms/interface.js
+++ src/cms/interface.js
@@ -1,22 +1,23 @@
 import { request, gql } from "graphql-request";
 
 const ENDPOINT =
-  "https://api-us-east-1.graphcms.com/v2/ckokdizpoj98w01xs12m2f7n4/master";
+  "https://api-us-east-1.graphcms.com/v2/ckrxg84xq19bk01z8agl1b9dr/master";
 
 const portfolioProjects = gql`
   {
-    portfolioProjects {
-      title
+    projects {
+      blog
       description
-      blogLink
-      imageLink
-      githubRepo
+      github
+      image
       live
+      title
     }
   }
 `;
 
 export const getProjects = async () => {
   const response = await request(ENDPOINT, portfolioProjects);
+  console.log(response)
   return await response;
 };
